3af9171d343d4e08dd5d81b2ea0cc0c78024e624
René Herrmann <rene@herrmannplatz.de>
2013-11-27 23:51:58 +0100

    formatting and canvas warning

--- cutout.js
+++ cutout.js
@@ -4,16 +4,15 @@
 	// check for canvas
 	var canvasSupported = !!window.HTMLCanvasElement;
 
-	// cache served images
+	// cache extraced images
 	var cache = {};
 
 	// convert image to base64
 	function getBase64Image(img, offsetX, offsetY, width, height) {
-	  	
 	  	var cvs = document.createElement("canvas");
 	  	var ctx = cvs.getContext("2d");
 	  	var x = offsetX || 0;
-	 	var	y = offsetY || 0;
+	 	var y = offsetY || 0;
 
 	 	cvs.width = width || img.width;
 		cvs.height = height || img.height;
@@ -24,10 +23,10 @@
 	// extract background styles and export as base64
 	function exportImage(img, style, callback) {
 		var position = style.backgroundPosition.split(" ");
-		var	x = Math.abs(parseInt(position[0]));
-		var	y = Math.abs(parseInt(position[1]));
-		var	width = parseInt(style.width);
-		var	height = parseInt(style.height);
+		var x = Math.abs(parseInt(position[0]));
+		var y = Math.abs(parseInt(position[1]));
+		var width = parseInt(style.width);
+		var height = parseInt(style.height);
 		return getBase64Image(img,x,y, width, height); 
 	}
 
@@ -42,7 +41,7 @@
 		var div = document.createElement('div');
 		div.className = selector;
 		div.style.display = 'none';
-		document.body.appendChild(div); // clean up missing
+		document.body.appendChild(div); 
 
 		var style = div.currentStyle || window.getComputedStyle(div, null);
 		var url = style.backgroundImage.slice(4, -1);
@@ -59,6 +58,8 @@
 	// expose
 	if(!window.cutout && canvasSupported) {
 		window.cutout = cutout;
+	} else {
+		console.warn('cutout.js will not work without Canvas');
 	}
 
 })();
