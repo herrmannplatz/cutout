3eda8f64a6d32d5f03a8b92a62fa5e3b4ae330b3
herrmannplatz <rene@herrmannplatz.de>
2014-01-19 23:01:00 +0100

    jslint + canvas check

--- cutout.js
+++ cutout.js
@@ -1,71 +1,72 @@
+/*jslint es5: true, indent: 4 */
+/*global window, document, Image */
 
-(function(){
-	"use strict";
+// check for canvas
+if (!!window.HTMLCanvasElement) {
 
-	// check for canvas
-	var canvasSupported = !!window.HTMLCanvasElement;
+    (function() {
+        "use strict";
 
-	// cache served images
-	var cache = {};
+        // cache served images
+        var cache = {};
 
-	// convert image to base64
-	function getBase64Image(img, offsetX, offsetY, width, height) {	
-		var cvs = document.createElement("canvas");
-		var ctx = cvs.getContext("2d");
-		var x = offsetX || 0;
-		var y = offsetY || 0;
+        // convert image to base64
+        function getBase64Image(img, offsetX, offsetY, width, height) {
+            var cvs = document.createElement("canvas"),
+                ctx = cvs.getContext("2d"),
+                x = offsetX || 0,
+                y = offsetY || 0;
 
-		cvs.width = width || img.width;
-		cvs.height = height || img.height;
-		ctx.drawImage(img, x, y, cvs.width, cvs.width, 0, 0, cvs.width, cvs.width); 
-		return cvs.toDataURL("image/png");
-	}
+            cvs.width = width || img.width;
+            cvs.height = height || img.height;
+            ctx.drawImage(img, x, y, cvs.width, cvs.width, 0, 0, cvs.width, cvs.width);
+            return cvs.toDataURL("image/png");
+        }
 
-	// extract background styles and export as base64
-	function exportImage(img, style) {
-		var position = style.backgroundPosition.split(" ");
-		var x = Math.abs(parseInt(position[0]));
-		var y = Math.abs(parseInt(position[1]));
-		var width = parseInt(style.width);
-		var height = parseInt(style.height);
-		return getBase64Image(img,x,y, width, height); 
-	}
+        // extract background styles and export as base64
+        function exportImage(img, style) {
+            var position = style.backgroundPosition.split(" "),
+                x = Math.abs(parseInt(position[0], 10)),
+                y = Math.abs(parseInt(position[1], 10)),
+                width = parseInt(style.width, 10),
+                height = parseInt(style.height, 10);
+            return getBase64Image(img, x, y, width, height);
+        }
 
-	// api 
-	function cutout(selector, callback) {
-		
-		if(cache[selector]) {
-			callback(cache[selector]);
-			return;
-		}
+        // api 
+        function cutout(selector, callback) {
+            if (cache[selector]) {
+                callback(cache[selector]);
+                return;
+            }
 
-		var div = document.createElement('div');
-		div.className = selector;
-		div.style.display = 'none';
-		document.body.appendChild(div); 
+            var div = document.createElement('div'),
+                style, url, img;
 
-		var style = window.getComputedStyle(div, null);
-		var url = style.backgroundImage.slice(4, -1).replace(/"/g, "");
+            div.className = selector;
+            div.style.display = 'none';
+            document.body.appendChild(div);
 
-		var img = new Image();
-		img.onload = function(e) {
-			cache[selector] = exportImage(img, style); // cache image
-			document.body.removeChild(div);
-			callback(cache[selector]);
-		};
-		img.onerror = function(e) {
-			document.body.removeChild(div);
-			callback(cache[selector]);
-		};
-		img.src = url;
-	}
+            style = window.getComputedStyle(div, null);
+            url = style.backgroundImage.slice(4, -1).replace(/"/g, "");
+            img = new Image();
 
-	// expose
-	if(!window.cutout && canvasSupported) {
-		window.cutout = cutout;
-	} else {
-		console.warn('cutout.js will not work without canvas');
-    }
+            img.onload = function() {
+                cache[selector] = exportImage(img, style); // cache image
+                document.body.removeChild(div);
+                callback(cache[selector]);
+            };
+            img.onerror = function() {
+                document.body.removeChild(div);
+                callback(cache[selector]);
+            };
+            img.src = url;
+        }
 
-})();
+        // expose
+        window.cutout = window.cutout || cutout;
+
+    })();
+
+}
 
--- jquery.cutout.js
+++ jquery.cutout.js
@@ -1,78 +1,78 @@
-(function($) {
+/*global window, document, $ , console */
 
-	var canvasSupported = !!window.HTMLCanvasElement;
+// check for canvas
+if (!!window.HTMLCanvasElement) {
 
-	// cache served images
-	var cache = {};
+    (function() {
+        "use strict";
 
-	// convert image to base64
-	function getBase64Image(img, offsetX, offsetY, width, height) {	
-		var cvs = document.createElement("canvas");
-		var ctx = cvs.getContext("2d");
-		var x = offsetX || 0;
-		var y = offsetY || 0;
+        // cache served images
+        var cache = {};
 
-		cvs.width = width || img.width;
-		cvs.height = height || img.height;
-		ctx.drawImage(img, x, y, cvs.width, cvs.width, 0, 0, cvs.width, cvs.width); 
-		return cvs.toDataURL("image/png");
-	}
- 	
- 	// TODO: options
-    function cutout(selector) {
+        // convert image to base64
+        function getBase64Image(img, offsetX, offsetY, width, height) {
+            var cvs = document.createElement("canvas"),
+                ctx = cvs.getContext("2d"),
+                x = offsetX || 0,
+                y = offsetY || 0;
 
-    	var self = this,
-    		counter = 0,
-    		div,
-    		pos,
-    		x, y,
-    		w, h;
+            cvs.width = width || img.width;
+            cvs.height = height || img.height;
+            ctx.drawImage(img, x, y, cvs.width, cvs.width, 0, 0, cvs.width, cvs.width);
+            return cvs.toDataURL("image/png");
+        }
 
-    	// check cache
-    	if(cache[selector]) {
-    		console.log('cached');
-    		return self.css({
-				'background-image' : 'url(' + cache[selector] + ')'
-			});
-		}
+        //TODO: optionsj
+        function cutout(selector) {
 
-		// create temporary div
-		div = $('<div></div>')
-			.addClass(selector)
-			.insertBefore($('body'))
-			.hide();
-		
-		// extract sprite properties
-		url = div.css('background-image').slice(4, -1).replace(/"/g, "");
-		pos = div.css('background-position').split(" ");
-		x = Math.abs(parseInt(pos[0]));
-		y = Math.abs(parseInt(pos[1]));
-		width = div.width();
-		height = div.height();
+            var self = this,
+                url, div, pos, x, y, w, h;
 
-		// clean up
-		div.remove();
+            // check cache
+            if (cache[selector]) {
+                console.log('cached');
+                return self.css({
+                    'background-image' : 'url(' + cache[selector] + ')'
+                });
+            }
 
-		// load sprite sheet
-		$('<img/>')
-			.load(function(e) {
-				cache[selector] = getBase64Image(this,x,y, width, height);
-				self.css({
-					'background-image' : 'url(' + cache[selector] + ')'
-				});
-			})
-			.error(function(e) {
-				console.warn('unable to get image from class ' + selector);
-			})
-			.attr({ src : url });
+            // create temporary div
+            div = $('<div></div>')
+                .addClass(selector)
+                .insertBefore($('body'))
+                .hide();
 
-		return self;
-    }
+            // extract sprite properties
+            url = div.css('background-image').slice(4, -1).replace(/"/g, "");
+            pos = div.css('background-position').split(" ");
+            x = Math.abs(parseInt(pos[0], 10));
+            y = Math.abs(parseInt(pos[1], 10));
+            w = div.width();
+            h = div.height();
 
-    if(!$.fn.cutout && canvasSupported) {
-		$.fn.cutout = cutout;
-	} else {
-		console.warn('cutout.js will not work without canvas');
-    }
- 
-}(jQuery));
+            // clean up
+            div.remove();
+
+            // load sprite sheet
+            $('<img/>')
+                .load(function() {
+                    cache[selector] = getBase64Image(this, x, y, w, h);
+                    self.css({
+                        'background-image' : 'url(' + cache[selector] + ')'
+                    });
+                })
+                .error(function() {
+                    console.warn('unable to get image from class ' + selector);
+                })
+                .attr({ src : url });
+
+            return self;
+        }
+
+        if (!$.fn.cutout) {
+            $.fn.cutout = cutout;
+        }
+
+    }());
+
+}
